2834f1b0db336c5f876a102a9d7366545199c091
Sumit Kumar <sumit@Sumits-MacBook-Air.local>
2026-04-06 11:59:30 +0530

    send Request

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,7 +1,25 @@
+import axios from "axios";
 import React from "react";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { removeUserFromFeed } from "../utils/feedSlice";
 
 const UserCard = ({ user }) => {
-  const { firstName, lastName, age, about, gender, skills, photoUrl } = user;
+  const { _id, firstName, lastName, age, about, gender, skills, photoUrl } =
+    user;
+  const dispatch = useDispatch();
+  const sendRequest = async (status, userId) => {
+    try {
+      await axios.post(
+        BASE_URL + "/request/send/" + status + "/" + userId,
+        {},
+        { withCredentials: true },
+      );
+      dispatch(removeUserFromFeed(userId));
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     user && (
       <div className="card bg-base-300 w-96 shadow-sm">
@@ -13,8 +31,18 @@ const UserCard = ({ user }) => {
           {age && gender && <p>{age + ", " + gender}</p>}
           <p>{about}</p>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Ignore</button>
-            <button className="btn btn-secondary">Interested</button>
+            <button
+              className="btn btn-primary"
+              onClick={() => sendRequest("ignored", _id)}
+            >
+              Ignore
+            </button>
+            <button
+              className="btn btn-secondary"
+              onClick={() => sendRequest("interested", _id)}
+            >
+              Interested
+            </button>
           </div>
         </div>
       </div>
